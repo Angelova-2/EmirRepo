d6e58aa7054887e77ab67a7a80b7367cc2c08452
kambeshhh <ekambesh2009@gmail.com>
2026-06-02 09:30:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace Emir05
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+       private T[] elements;
+        public int Count { 
+            count.get
+            
+            
+            
+        
+        
+        
+        }
+        private const int InitialCapacity = 16;
+
+        public ArrayStack(int capacity = InitialCapacity) 
+        { 
+         ArrayStack = this.elements.Length;
+        
+        
+            
+
+        }
+        public void Push(T element) 
+        {
+        Push(element, this.Count);
+        Push(element,Grow()) = this.elements.Length;
+        
+        }
+        public T Pop() 
+        {
+        Pop() = this.Count-- && Pop().this.elements;
+              if (this.Count == 0) 
+            {
+                throw new InvalidOperationException();
+            }
+            
+            this.elements = newElements;
+        }
+        public T[] ToArray() 
+        {
+         this.elements = new T(this.Count);
+            return this.elements;
+            this.Count = 0;
+            return this.Count -1;
+            
+        }
+        
+        private void Grow() 
+        {
+            T[] newElements;
+            
+ 
+
+
+
+        }
+        
+
+
+
+
+    }
+
+    }
+}
+
